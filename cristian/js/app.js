d4a58a53535cf6c1708616c8b87d6fff919f6991
Angularjs <angularjs@ZiMac03.local>
2017-10-07 11:27:47 +0200

    ngRoute

--- cristian/js/app.js
+++ cristian/js/app.js
@@ -1,3 +1,17 @@
 
       //App
-      angular.module('app', []);
+      angular.module('app', ['ngRoute'])
+
+      .config(function($routeProvider) {
+
+      	$routeProvider
+      		.when("/", {
+      			templateUrl : "views/main.html"
+      		})
+      		.when("/red", {
+      			templateUrl : "views/red.html"
+      		})
+      		.when("/green", {
+      			templateUrl : "views/green.html"
+      		});
+      });
