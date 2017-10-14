9395debb1b3dee497b1a1768c891f6b0866f6c29
Carlos <carlos@ZiMacMaster.local>
2017-10-14 14:59:42 +0200

    Clase 3

--- Ejemplos/buses-v2/gulpfile.js
+++ Ejemplos/buses-v2/gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('serve', function() {
   gulp.src('./')
     .pipe(webserver({
       host: '0.0.0.0',
-      port: 3000,
+      port: 8080,
       fallback: 'index.html',
       livereload: true
     }))
