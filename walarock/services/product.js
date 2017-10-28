a0f542683b923993bc4611f7d31ae88785d5049e
Angularjs <angularjs@ZiMac02.local>
2017-10-28 13:19:09 +0200

    Product service functions

--- walarock/services/product.js
+++ walarock/services/product.js
@@ -6,24 +6,73 @@ function productService($http) {
 	console.log('Product Service instantiated!')
 
 	var product = {
-		list: [],
+		list: {},
+		init: init,
 		get: get,
 		add: add,
-		clear: clear
+		clear: clear,
+		getKeys: getKeys
 	};
 
 	return product;
 
-	function get() {
-		return;
+	function init() {
+		return product.list = {
+			1: {
+				"title": "product 1",
+				"description": "description 1",
+				"photos": [],
+				"price": 10.0
+			},
+			2: {
+				"title": "product 2",
+				"description": "description 2",
+				"photos": [],
+				"price": 20.0
+			}
+		};
 	}
 
-	function add() {
-		return;
+	function get(productId) {
+		return product.list[productId];
+	}
+
+	function add(productObject) {
+		// Get new unique Id for product
+		var productId = getNewId();
+
+		product.list[productId] = productObject;
 	}
 
 	function clear() {
-		product.list = [];
+		product.list = {};
+	}
+
+    function getKeys() {
+    	// Get all keys in the products catalog
+		var keys = [];
+		for(var k in product.list) keys.push(k); 
+		return keys;   	
+    }
+
+	function getNewId() {
+		// Set min and max values for productId
+		var min = 1;
+		var max = 10000000;
+
+		// Get the current productIds in the catalog
+    	var current_keys = product.getKeys();
+
+    	// Inizialize new_key
+    	var new_key = 0;
+
+    	// Repeat until obtain a unique productId
+    	while (!new_key || new_key in current_keys) {
+    		// Get new random productId
+    		new_key = Math.floor(Math.random() * (max - min + 1)) + min;
+    	}
+
+    	return new_key;
 	}
 
 }
