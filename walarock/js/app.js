8c42b731d261f1da3a7bc135aa68f625a2a3b400
CodingCarlos <carloshdmn@gmail.com>
2017-10-28 13:06:02 +0200

    Add 404 and ng-view

--- walarock/js/app.js
+++ walarock/js/app.js
@@ -11,5 +11,8 @@ angular.module('app').config(function($routeProvider) {
 		})
 		.when("/list", {
 			templateUrl: "views/list.html"
+		})
+		.otherwise({
+			template : '<h1>Error 404</h1><a href="#/list">Go to list</p>'
 		});
 });
