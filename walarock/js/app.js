a8db6ff80cf742a2acfdeb50af82c29de821a838
Angularjs <angularjs@ZiMac02.local>
2017-10-28 13:33:08 +0200

    Fix angular router

--- walarock/js/app.js
+++ walarock/js/app.js
@@ -13,6 +13,6 @@ angular.module('app').config(function($routeProvider) {
 			templateUrl: "views/product.html"
 		})
 		.otherwise({
-			template : '<h1>Error 404</h1><a href="#/list">Go to list</p>'
+			template : '<h1>Error 404</h1><a href="#!/list">Go to list</p>'
 		});
 });
