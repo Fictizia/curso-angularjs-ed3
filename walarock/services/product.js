fb6579a3666196ee162937f7525df718e2494530
Angularjs <angularjs@ZiMac01.local>
2017-11-04 12:51:39 +0100

    add firebase

--- walarock/services/product.js
+++ walarock/services/product.js
@@ -1,17 +1,18 @@
 // Services
-angular.module('app').service('productService', ['$http', productService]);
+angular.module('app').service('productService', ['$http', '$firebaseArray', '$firebaseObject', productService]);
 
-function productService($http) {
+function productService($http, $firebaseArray, $firebaseObject) {
 
 	console.log('Product Service instantiated!')
 
 	var product = {
-		list: {},
+		list: [],
+		objectList: {},
 		init: init,
 		get: get,
 		add: add,
 		clear: clear,
-		getKeys: getKeys
+		//getKeys: getKeys
 	};
 
 	init();
@@ -19,62 +20,53 @@ function productService($http) {
 	return product;
 
 	function init() {
-		return product.list = {
-			"1": {
-				"title": "product 1",
-				"description": "description 1",
-				"photos": ['https://cloud.google.com/images/pricing/free.svg'],
-				"price": 10.0
-			},
-			"2": {
-				"title": "product 2",
-				"description": "description 2",
-				"photos": [],
-				"price": 20.0
-			}
-		};
+		var ref = firebase.database().ref('walarock/products');
+		product.list = $firebaseArray(ref);
+		product.objectList = $firebaseObject(ref);
+		console.log('list');
 	}
 
 	function get(productId) {
-		return product.list[productId];
+		return product.objectList[productId];
 	}
 
 	function add(productObject) {
 		// Get new unique Id for product
-		var productId = getNewId();
+		//var productId = getNewId();
 
-		product.list[productId] = productObject;
+		//product.list[productId] = productObject;
+		product.list.$add(productObject);
 	}
 
 	function clear() {
 		product.list = {};
 	}
 
-    function getKeys() {
-    	// Get all keys in the products catalog
-		var keys = [];
-		for(var k in product.list) keys.push(k); 
-		return keys;   	
-    }
+  //   function getKeys() {
+  //   	// Get all keys in the products catalog
+		// var keys = [];
+		// for(var k in product.list) keys.push(k); 
+		// return keys;   	
+  //   }
 
-	function getNewId() {
-		// Set min and max values for productId
-		var min = 1;
-		var max = 10000000;
+	// function getNewId() {
+	// 	// Set min and max values for productId
+	// 	var min = 1;
+	// 	var max = 10000000;
 
-		// Get the current productIds in the catalog
-    	var current_keys = product.getKeys();
+	// 	// Get the current productIds in the catalog
+ //    	var current_keys = product.getKeys();
 
-    	// Inizialize new_key
-    	var new_key = 0;
+ //    	// Inizialize new_key
+ //    	var new_key = 0;
 
-    	// Repeat until obtain a unique productId
-    	while (!new_key || new_key in current_keys) {
-    		// Get new random productId
-    		new_key = Math.floor(Math.random() * (max - min + 1)) + min;
-    	}
+ //    	// Repeat until obtain a unique productId
+ //    	while (!new_key || new_key in current_keys) {
+ //    		// Get new random productId
+ //    		new_key = Math.floor(Math.random() * (max - min + 1)) + min;
+ //    	}
 
-    	return new_key;
-	}
+ //    	return new_key;
+	// }
 
 }
